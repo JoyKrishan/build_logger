b9eb593430f7782f993ba09c75e9829385f18418
Joy Krishan Das <joykrishan10@gmail.com>
2025-07-23 15:52:08 -0600

    Fix: Add missing log collection from SoftwareFactory CI and update README

--- README.md
+++ README.md
@@ -1,33 +1,45 @@
-# Exercise: Downloading Build Logs from OpenDev Code Review
+# Exercise Report: Downloading Build Logs from OpenDev Code Review
 
 Build Logger to download all build log files from OpenDev code review record: https://review.opendev.org/c/openstack/watcher-tempest-plugin/+/954214. 
 
-**all_logs.zip** contains all the logs from the builds in the code review.
+**all_logs.zip** - Contains all logs downloaded from the builds in the code review.
 
 ## Steps to Reproduce
 
 1. **Clone the repository**
 2. **Install dependencies**
     ```bash
-    python3 -m venv venv
-    source venv/bin/activate     
+    python3 -m venv .venv
     pip install -r requirements.txt
     pip install -e .
     ```
-3. **Download logs**
+3. **Run the Script**
     ```bash
     build_logger -d 'path/to/download/directory'
     ```
 
-## Directory Structure 
+How it looks when running:
+![alt text](static_files/running_instance.png) 
 
-Logs are saved in the following format:
-```
-<path_to_download_directory>/<comment_id>/builds/<zuul_build_uuid>/
-```
-This is done to track logs by the comment ID of the code review and the unique build UUID.
+## Logs Organization
 
-For example, if your download directory is `all_logs`, logs will be stored in:
+Logs are saved in this folder structure:
 ```
-all_logs/<comment_id>/builds/<zuul_build_uuid>/
+<path>/<comment_id>/builds/<build_uuid>/
 ```
+
+For example:
+```
+all_logs/2fec73a50ccb11733556f6d717ca16dbdba9e47b/builds/c83de12787bf4f91aa9829f5d5d30b4d/
+```
+This organization links each comment (comment_id) with its corresponding build logs. I could have organized by build outcome and job name, or created a CSV to track details, but preferred this simple structure to track the relationship between comment_id and the builds.
+
+
+*Rationale on why all files under log tab are downloaded*: According to [Zuul documentation](https://zuul-ci.org/docs/zuul/latest/job-content.html#working-directory), the Zuul executor creates a directory containing all content related to a job. Therefore, downloading everything under the Log tab gives complete information about the build/test process and environment.
+
+![alt text](static_files/zuul_ui.png)
+
+*Disclosure of Help Taken*
+- I utilized GitHub Copilot's autocomplete feature in Visual Studio Code while writing this script. 
+- I did not use Copilot's chat interface or Copilot agent, so there are no chat logs.
+- I have referred to public information on the internet to learn how to download logs from Zuul CI. To be specific, I used the following resource: [https://opendev.org/zuul/zuul-jobs/commit/ca2ee69e60accf0292b9e3fdb4605b51be646525]
--- build_logger/get_build_logs.py
+++ build_logger/get_build_logs.py
@@ -1,7 +1,6 @@
 import os
 import gzip
 import json
-import sys
 import argparse
 import subprocess
 import shutil
@@ -14,6 +13,7 @@ import requests
 REVIEW_URL = "https://review.opendev.org/c/openstack/watcher-tempest-plugin/+/954214"
 ZUUL_API= "https://zuul.opendev.org/api/tenant/openstack"
 OPENDEV_API = "https://review.opendev.org/changes"
+SOFTWAREFACTORY_API = "https://softwarefactory-project.io/zuul/api/tenant/rdoproject.org"
 
 def get_change_details():
     change_id = REVIEW_URL.split("/")[-1]
@@ -31,19 +31,19 @@ def get_change_details():
 def extract_build_links(comments):
     build_links = []
     for comment in comments:
-        comment_id = comment['id']
-        comment_text = comment['message']
-        author_username = comment['author']['username']
-        tag= comment.get('tag', '')
-        # Checks if the comment is by Zuul
-        if len(tag.split(':')) > 1 and tag.split(':')[1] == 'zuul' and author_username and author_username.lower() == 'zuul':
+        comment_id = comment.get('id', '')
+        comment_text = comment.get('message', '')
+        author_username = comment.get('author', {}).get('username', '')
+        author_name = comment.get('author', {}).get('name', '')
+
+        if (author_username.lower() == 'zuul' and author_name.lower() == 'zuul') or (author_username.lower() == 'sf-project-io' and author_name.lower() == 'software factory ci'):
             lines = comment_text.split('\n')
             for line in lines:
                 if line.startswith('- '): 
                     line = line.strip().split(' ')
-                    if len(line) == 8 and line[2].startswith('https://zuul.opendev.org/t/openstack/build/'):
-                        # TODO: Track all info (i.e., job_name, outcome, build_time) in the build link with a CSV file
+                    if len(line) == 8 and line[2].startswith('https://zuul.opendev.org/t/openstack/build/') or line[2].startswith('https://softwarefactory-project.io/zuul/t/rdoproject.org/build/'):
                         build_links.append({
+                            'ci': author_username.lower(),
                             'comment_id': comment_id,
                             'url': line[2],
                             'job_name': line[1],
@@ -55,8 +55,15 @@ def extract_build_links(comments):
     
     return build_links
 
-def collect_all_log_files(zuul_uuid):
-    zuul_api_url = f"{ZUUL_API}/build/{zuul_uuid}"
+def collect_all_log_files(uuid, ci):
+    if ci == 'zuul':
+        zuul_api_url = f"{ZUUL_API}/build/{uuid}"
+    elif ci == 'sf-project-io':
+        zuul_api_url = f"{SOFTWAREFACTORY_API}/build/{uuid}"
+    else:
+        print(f"Unknown CI system: {ci}")
+        return 
+    
     try:
         base_url = urllib.request.urlopen(zuul_api_url).read()
         base_json = json.loads(base_url)
@@ -68,15 +75,15 @@ def collect_all_log_files(zuul_uuid):
             manifest_json = json.loads(manifest.read())
     except HTTPError as e:
         if e.code == 404:
-            print("Could not find build UUID in Zuul API. This can happen with ", file=sys.stderr)
+            print("Could not find build UUID in Zuul API. This can happen with")
         else:
-            print(e, file=sys.stderr)
-        sys.exit(2)
+            print(e)
+        return
         
     def collect_files(node, parent):
         files = []
         if node.get('mimetype') != 'application/directory':
-            files.append(parent+node['name'])
+            files.append(parent+node['name'].split(' ')[0])
             
         if node.get('children'):
             for child in node.get('children'):
@@ -95,9 +102,9 @@ def save_log_files(file_path, base_url, save_dir):
         xtra_args = []
     
     cmd = ['curl', '-s', '--globoff'] + xtra_args + ['--create-dirs', '-o', file_path, base_url + file_path]
-    result = subprocess.run(cmd, cwd=save_dir)
+    result = subprocess.run(cmd, cwd=save_dir, capture_output=True, text=True)
     if result.returncode != 0:
-        print(f"Failed to download {base_url}{file_path}; skipping")
+        print(f"Failed to download {base_url}/{file_path} skipping")
         print(f"Error: {result.stderr}")
         return
     
@@ -125,14 +132,15 @@ def main():
             shutil.rmtree(download_path, ignore_errors=True) # remove previous logs
             for build_info in tqdm(all_build_links, desc="Processing builds", position=0, leave=True, colour='green'):
                 comment_id = build_info['comment_id']
-                zuul_uuid = build_info['url'].split('/')[-1]
-                log_files = collect_all_log_files(zuul_uuid)
+                uuid = build_info['url'].split('/')[-1]
+                ci_username = build_info['ci']
+                log_files = collect_all_log_files(uuid, ci_username)
                 
-                save_dir = os.path.join(download_path, comment_id, 'builds', zuul_uuid)
+                save_dir = os.path.join(download_path, comment_id, 'builds', uuid)
                 os.makedirs(save_dir, exist_ok=True)
                 
                 base_log_url = log_files[0]
-                for log_file in tqdm(log_files[1:], desc=f"Downloading logs for build:{zuul_uuid}", position=1, leave=False):
+                for log_file in tqdm(log_files[1:], desc=f"Downloading logs for build:{uuid}", position=1, leave=False):
                     save_log_files(log_file, base_log_url, save_dir)  
             
             shutil.make_archive(f'{download_path.split("/")[-1]}', 'zip', download_path)
